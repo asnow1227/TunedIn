84ffc5b9fef71268d62814b9cb8f15b6b7a90f5d
Alexander Snow <alex@alexanders-mbp.mynetworksettings.com>
2022-12-13 17:30:21 -0500

    tutorial 6

--- tuned_in/api/serializers.py
+++ tuned_in/api/serializers.py
@@ -6,3 +6,10 @@ class RoomSerializer(serializers.ModelSerializer):
         model = Room
         fields = ('id', 'code', 'host', 'guest_can_pause', 
             'votes_to_skip', 'created_at')
+
+
+class CreateRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        #ensures that when we send a post request, these fields are handled and included
+        fields = ('guest_can_pause', 'votes_to_skip')
